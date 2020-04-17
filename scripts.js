02ce7dfaa579fbcc6ddc0013834d8263e719a3c4
dianestephani <diane.stephani@gmail.com>
2020-04-16 21:46:23 -0700

    Created WA state prompt

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
-var greeting = 'Welcome, ';
-var name = 'Washington State!';
-var welcome = greeting + name;
- var date = new Date();
-var day = date.getDay()
-var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-console.log("Today is " + daylist[day] + "!");
+// var greeting = 'Welcome, ';
+// var name = 'Washington State!';
+// var welcome = greeting + name;
+//  var date = new Date();
+// var day = date.getDay()
+// var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// console.log("Today is " + daylist[day] + "!");
 
-function javaDay() {
-    document.write (welcome + '</br>');
-    document.write ("Today is " + daylist[day] + "!");
+// function javaDay() {
+//     // document.write (welcome + '</br>');
+//     // document.write ("Today is " + daylist[day] + "!");
+// }
+// while (false) {
+//     console.log ("I'm here again!")
+// }
+// prompt("Do you live in Washington state? Yes or no.")
+// console.log("I'm here!")
+
+var response = prompt("Do you live in Washington state? Yes or no.").toLowerCase();
+console.log(response) 
+if( response === 'yes' || response === 'no' ) { 
+    alert('Thank you for your response!');
 }
